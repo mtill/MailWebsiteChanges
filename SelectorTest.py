35093e2ca0280acf34fd00435340aa9f0124d886
Debian Guru <Debianguru@gmx.de>
2013-09-21 12:15:37 +0200

    .

--- SelectorTest.py
+++ SelectorTest.py
@@ -6,5 +6,5 @@ import MailWebsiteChanges
 # URI | content type | XPath | regular expression | encoding
 site = sys.argv   # invoke this script with e.g., "http://my-website-uri123.org/download/" "html" "XPath-Selector" "regular expression" "encoding"
 
-print MailWebsiteChanges.parseSite(site[1], site[2], site[3], site[4], site[5])[0]
+print MailWebsiteChanges.parseSite(site[1], site[2], site[3], site[4], site[5])['content']
 
