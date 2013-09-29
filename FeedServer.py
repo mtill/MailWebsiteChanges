ab8eddf23ebcc692b8395de3fd86a7ac530cbd7f
Debian Guru <Debianguru@gmx.de>
2013-09-29 17:53:02 +0200

    command line arguments

--- FeedServer.py
+++ FeedServer.py
@@ -5,12 +5,26 @@ import http.server
 import socketserver
 import importlib
 import sys
+import getopt
 
 port = 8000
-
 configMod = 'config'
-if (len(sys.argv) > 1):
-        configMod = sys.argv[1]
+
+
+try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hc:p:', ['help', 'config=', 'port='])
+except getopt.GetoptError:
+        print('Usage: FeedServer.py --config=config --port=8000')
+        sys.exit(1)
+for opt, arg in opts:
+        if opt == '-h':
+                print('Usage: FeedServer.py --config=config --port=8000')
+                exit()
+        elif opt in ('c', '--config'):
+                configMod = arg
+        elif opt in ('-p', '--port'):
+                port = int(arg)
+
 config = importlib.import_module(configMod)
 
 
