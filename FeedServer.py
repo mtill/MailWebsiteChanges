28f41818964732cfd50a331d163ec4ac1cb47dbd
Debian Guru <Debianguru@gmx.de>
2013-09-29 16:19:14 +0200

    bugfix

--- FeedServer.py
+++ FeedServer.py
@@ -3,8 +3,16 @@
 import config
 import http.server
 import socketserver
+import importlib
+import sys
 
 port = 8000
+
+configMod = 'config'
+if (len(sys.argv) > 1):
+        configMod = sys.argv[1]
+config = importlib.import_module(configMod)
+
 
 handler = http.server.SimpleHTTPRequestHandler
 
